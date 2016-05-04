f85ae525923eb909291b3debac9ecf7b29e5cf49
babirondo <babirondo@gmail.com>
2016-05-04 03:13:46 -0300

    listando os workflows via api

---
+++ classes/globais.php
@@ -0,0 +1,4 @@
+<?php
+$SERVER_API = "localhost:8080/CWapi/";
+
+?>
---
+++ index.php
@@ -0,0 +1,46 @@
+<?php 
+error_reporting(E_ALL );
+
+require_once("classes/globais.php");
+require_once("classes/diversos.php");
+ 
+
+$retorno = CallAPI("get", $SERVER_API."getWorkflows/");
+
+?>
+
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<table border=1 cellspacing=0 cellpadding=0  width=100% height=100%>
+			<tr  height=15%>
+				<td>cabecalho</td>
+			</tr>
+			<tr height=5%>
+				<td>
+					<table border=0 width=100% >
+					  <tr>
+					  <?php 
+					  foreach ($retorno[FETCH] as $linha){
+					  	echo "<TD>". $linha["workflow"]."</td>";
+					  	
+					  }
+					  ?>
+					  </tr>
+					</table>
+				</td>
+			</tr>
+			<tr height=5%>
+				<td>menu</td>
+			</tr>
+			<tr >
+				<td>corpo</td>
+			</tr>
+			<tr  height=10%>
+				<td>footer</td>
+			</tr>
+		</table>
+	</body>
+</html>
