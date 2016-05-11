5e4de9e891e055400816b5a4729b849397b94d3e
babirondo <babirondo@gmail.com>
2016-05-11 02:57:38 -0300

    campos nulos criticados, corrigida rotina de traducao dos campos para mandar email, exibindo o staus do processo e a data da ultima movimentacao

--- form.php
+++ form.php
@@ -1,54 +1,102 @@
 <form action="<?=$PHP_SELF;?>" method=post>
 	<input type=hidden name=processar value=1>
 	
-<?php 
+<?php  
 if ($_POST["processar"]==1)
 {
-	// TODO exibir titulo da pagina
-	foreach ($_POST["idcampoposto"] as $key => $result)
-	{
-		$array[$key][idpostocampo]  = $key;		
-		$array[$key][valor]  = $result;		
-		
-		$array[$key][idworkflowdado]  = $_POST["idworkflowdado"][$key];
-	}
-	$array[processo][valor]  = $_POST["processo"];	
-	$array[processo][acao]  = $_POST["finalizar"];
-	$array[processo][idworkflowtramitacao_original]  = $_POST["H"];
-	
-	$registering = CallAPI("POST", $SERVER_API."Registrar/".$_GET["idworkflow"]."/".$_GET["idposto"] , json_encode( $array) );
-	
+//        echo "<pre>"; var_dump($_POST);echo "</pre>";
+
+        // validando campos antes de enviar
+        $msg_erro = null;
+        foreach ( $_POST["idcampoposto"] as $idcampo => $valor){
+            
+            $Restore[$idcampo][valor_postado] = $valor;
+            IF ($_POST["obrigatorio"][$idcampo] == 1){
+
+                IF ( empty( $_POST["idcampoposto"][$idcampo])  ) {
+                    $Erro[$idcampo][erro] = "Campo Obrigatório";
+                }
+            }
+        }
+     //   echo "<pre>"; var_dump($Erro);echo "</pre>";
+        if (!is_array($Erro))
+        {
+            // TODO exibir titulo da pagina
+            foreach ($_POST["idcampoposto"] as $key => $result)
+            {
+                    $array[$key][idpostocampo]  = $key;		
+                    $array[$key][valor]  = $result;		
+
+                    $array[$key][idworkflowdado]  = $_POST["idworkflowdado"][$key];
+            }
+            $array[processo][valor]  = $_POST["processo"];	
+            $array[processo][acao]  = $_POST["finalizar"];
+            $array[processo][idworkflowtramitacao_original]  = $_POST["H"];
+
+            $registering = CallAPI("POST", $SERVER_API."Registrar/".$_GET["idworkflow"]."/".$_GET["idposto"] , json_encode( $array) );
 	//var_dump($registering);
 
-	if ($registering["resultado"] == "SUCESSO"){
-		echo " <font color='#ff0000'>Dados registrados com sucesso</font> ";
-	}
+            if ($registering["resultado"] == "SUCESSO"){
+                    echo " <font color='#ff0000'>Dados registrados com sucesso</font> ";
+            }
+        }
 	
 	echo "<BR> <font color='#ffaa00'><pre>".$registering["DEBUG"]."</pre></font> ";
-	 	
-	
 }
+        if (!is_array($msg_erro))
+             // só carrega os dados se não houve erro de validacao
+            $form = CallAPI("get", $SERVER_API.$_GET["idworkflow"]."/".(($_GET["processo"])?$_GET["processo"]:"0")."/getPosto/".$_GET["idposto"] );
+        
+            echo "<input type=hidden name=processo value='".$_REQUEST["processo"]."' >";
+            echo "<input type=hidden name=H value='".$_REQUEST["H"]."' >";
+
+            foreach ($form[FETCH_CAMPO] as $linha){
+                    $css=null;
+                    $exibir_erro=null;
+
+                    if ($Erro[$linha["idcampo"]][erro]){
+                        $css = " bgcolor=#aacccc";
+                        $exibir_erro = "<font color=#ff0000>".$Erro[$linha["idcampo"]][erro]."</font>";
+                    }
+                    if (is_array($Erro)){
+                        $linha["valor"] = $Restore[$linha["idcampo"]][valor_postado];
+                    }
+                    
 
 
-$form = CallAPI("get", $SERVER_API.$_GET["idworkflow"]."/".(($_GET["processo"])?$_GET["processo"]:"0")."/getPosto/".$_GET["idposto"] );
+                    echo "<tr $css>
+                            <TD>".(($linha["obrigatorio"])?"<font color=#ff0000>*</font>":""). $linha["campo"]."</td>
+                            <TD> ";
 
-	echo "<input type=hidden name=processo value='".$_GET["processo"]."' >";
-	echo "<input type=hidden name=H value='".$_GET["H"]."' >";
-		
-		foreach ($form[FETCH_CAMPO] as $linha){
-			echo "<tr>
-			    	<TD>". $linha["campo"]."</td>";
-			echo "  <TD> ".$linha["idcampo"]." <input type=text name=idcampoposto[". $linha["idcampo"]."] value='". $linha["valor"]."' size=30></td>
-	  		   </tr>	 ";
-			echo "<input type=hidden name=idworkflowdado[". $linha["idcampo"]."] value='". $linha["idworkflowdado"]."'>";
-		}
-		
-		echo "<tr>
+
+
+                    switch ($linha["inputtype"])
+                    {
+                        case("textarea"):
+                            echo "<textarea rows='". $linha["txtarea_rows"]."' cols='". $linha["txtarea_cols"]."' name=idcampoposto[". $linha["idcampo"]."]>". $linha["valor"]."</textarea>";
+                        break;
+                        default:
+                            echo " <input type=text size='". $linha["maxlenght"]."' name=idcampoposto[". $linha["idcampo"]."] value='". $linha["valor"]."'>";
+                    }
+
+                    echo " $exibir_erro
+                            </td>
+                       </tr>	 ";
+                    echo "<input type=hidden name=idworkflowdado[". $linha["idcampo"]."] value='". $linha["idworkflowdado"]."'>";
+                    echo "<input type=hidden name=obrigatorio[". $linha["idcampo"]."] value='". $linha["obrigatorio"]."'>";
+            }
+
+                echo "
+                    </table>
+                    <table>
+                        <tr>
 				<td><input type=button   value=' <<< Voltar'> </td> ";
-		if ($form[DADOS_POSTO][starter] != 1)
-			echo "<td><input type=submit name=finalizar value='Salvar'> </td>";
-		echo "			<td><input type=submit name=finalizar value='Salvar e Avançar >>>'> </td> 
-				</tr> ";
+		echo "          <td><input type=submit name=finalizar value='Salvar e Avançar >>>'> </td> ";
+                
+                if ($form[DADOS_POSTO][starter] != 1)
+                    echo "     <td><input type=submit name=finalizar value='Salvar'> </td>";
+
+                echo "</tr> ";
 		?>
 
 </form>
